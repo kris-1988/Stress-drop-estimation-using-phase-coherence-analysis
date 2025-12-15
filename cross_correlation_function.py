22d3b0c97521e007de52b3e1e5d7192ecb1723a0
kris-1988 <67629933+kris-1988@users.noreply.github.com>
2025-12-15 11:49:59 +0000

    Add files via upload

---
+++ cross_correlation_function.py
@@ -0,0 +1,48 @@
+import obspy
+import numpy as np
+
+def calc_xc(pick1, tr1, pick2, tr2, before_t_pick, after_t_pick, max_shift, filter=None, filter_option=None):
+
+    # bandpass filter
+    data1=tr1.copy()
+    if filter is not None:
+        data1.filter(filter=filter)
+    data2=tr2.copy()
+    
+    # grab data for the template trace
+    data1=data1.trim(starttime=pick1-before_t_pick,endtime=pick1+after_t_pick).data
+    # grab data for the second trace
+    data2=data2.trim(starttime=pick2-before_t_pick-max_shift,endtime=pick2+after_t_pick+max_shift).data
+
+    # normalize the template data
+    # LHS of denominator
+    data1=data1/np.sum(np.power(data1,2))**0.5
+
+    # cross correlate
+    # numerator
+    xc=np.correlate(data2,data1)
+
+    # normalizations of the second trace
+    # RHS of denominator
+
+    # length of template trace
+    N=data1.size
+
+    # cumulative sum of power in second trace
+    data2=np.cumsum(np.power(data2,2))
+    data2=np.append(0,data2)
+
+    # determine steps in cumulative sum in each interval
+    data2=data2[N:]-data2[0:-N]
+    data2=np.power(data2,0.5)
+
+    # normalize
+    xc=np.divide(xc,data2)
+
+    # note the pick time and the maximum xc value
+    imax=np.argmax(xc)
+    tshf=tr1.stats.delta*imax-max_shift
+    xc=[tshf,xc[imax]]
+
+    return xc
+ 
